1c9d81563c684a57b30613f714938ba462ca39ae
Pablo de Albuquerque <pablo.cda@gmail.com>
2023-02-02 16:56:43 -0300

    agrego uncompress

--- archiver.py
+++ archiver.py
@@ -2,7 +2,7 @@ import datetime
 import os
 import pdb
 from pathlib import Path
-from shutil import make_archive, rmtree
+from shutil import make_archive, rmtree, unpack_archive
 import argparse
 
 ARCDIR = 'rootfs/var/lib/sympa/arc'
@@ -17,24 +17,37 @@ parser.add_argument('--date', default='2019-02')
 args = parser.parse_args()
 
 def status(archive_path):
-    for archive in archive_path.iterdir():
+    for archive in sorted(archive_path.iterdir()):
         if archive.is_dir():
             print(archive.stem)
         if archive.suffix == '.zip':
             print(archive.stem + " COMPRESSED")
 
-def compress(archive_path, date):
+def need_procesing(date_input, archive):
+    date_archive = datetime.datetime.strptime(archive.stem, '%Y-%m')
+    return (date_input >= date_archive)
+
+
+def compress(archive_path, date_input):
     for archive in archive_path.iterdir():
         if archive.is_dir():
-            dt_archive = datetime.datetime.strptime(archive.stem, '%Y-%m')
-            if (date >= dt_archive):
+            if (need_procesing(date_input, archive)):
                 a = str(archive.resolve())
                 zip_file = make_archive(base_name=a, root_dir=a, verbose=True, format='zip')
                 if (zip_file): 
                     print(zip_file)
                     rmtree(archive)
     
-
+def uncompress(archive_path, date_input):
+    for archive in archive_path.iterdir():
+        if archive.suffix == '.zip':
+            if (need_procesing(date_input, archive)):
+                zip_file = str(archive.resolve())
+                archive_dir = f"{Path(archive_path).resolve()}/{archive.stem}"
+                os.makedirs(archive_dir, exist_ok=True)
+                print(f"{archive.stem} uncompressed")
+                unpack_archive(zip_file, extract_dir=archive_dir)
+                os.remove(zip_file)
 
 action = args.action
 list = args.list
@@ -48,6 +61,6 @@ dt = datetime.datetime.strptime(date, '%Y-%m')
 try:
     if action == 'status': status(archive_path)
     if action == 'compress': compress(archive_path, dt)
-    if action == 'uncompress': compress(archive_path, dt)
+    if action == 'uncompress': uncompress(archive_path, dt)
 except Exception as err:
     print('ERROR:', err)
